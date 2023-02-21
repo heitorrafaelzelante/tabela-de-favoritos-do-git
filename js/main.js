8013afe23a47ce834218a553c26b6707b396eef6
heitorrafaelzelante <heitorrafael.91@gmail.com>
2023-02-21 09:29:11 -0300

    tabela com montadad pelo java buscando usuario no git

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import { FavoritesView } from "./favorites.js";
+
+
+new FavoritesView("#app")
+
+
+
+
+
+ //this.entries = [
+      // {
+      //   login: 'maykbrito',
+      //   name: 'Mayk Brito',
+      //   public_repos: '76',
+      //   followers: '120000'
+      // },
+      // {
+      //   login: 'diego3g',
+      //   name: 'Diego Fernandes',
+      //   public_repos: '89',
+      //   followers: '148000'
+      // }
+   // ]
